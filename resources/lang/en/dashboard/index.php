ad18b339ce35829fdd14a54b0fce5f3d99f60051
WeeskyBDW <bdwadv@gmail.com>
2023-12-04 22:42:32 +0100

    Retrait de tout les fichiers
    On repart d'une base clean

--- resources/lang/en/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'search' => 'Rechercher des serveurs...',
-    'no_matches' => 'Aucun serveur ne correspond aux critères de recherche fournis.',
-    'cpu_title' => 'Processeur',
-    'memory_title' => 'Mémoire',
-];
