2baee16729e454261c38c217023f100adc916f8a
WeeskyBDW <36575072+WeeskyBDW@users.noreply.github.com>
2024-01-29 01:00:47 +0100

    adding default role feature (#4)
    work at back end of user creation, meaning that it (user) will have default role regardless if created by api, manually or with artisan (command-line)

--- app/Http/Controllers/Admin/Settings/IndexController.php
+++ app/Http/Controllers/Admin/Settings/IndexController.php
@@ -8,6 +8,7 @@ use Prologue\Alerts\AlertsMessageBag;
 use Illuminate\Contracts\Console\Kernel;
 use Illuminate\View\Factory as ViewFactory;
 use Pterodactyl\Http\Controllers\Controller;
+use Pterodactyl\Models\Role;
 use Pterodactyl\Traits\Helpers\AvailableLanguages;
 use Pterodactyl\Services\Helpers\SoftwareVersionService;
 use Pterodactyl\Contracts\Repository\SettingsRepositoryInterface;
@@ -37,6 +38,7 @@ class IndexController extends Controller
         return $this->view->make('admin.settings.index', [
             'version' => $this->versionService,
             'languages' => $this->getAvailableLanguages(true),
+            'roles' => Role::all(),
         ]);
     }
 
